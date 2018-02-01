350c07cfc7976f2b27f685c234351f7597843809
Tahmid Sadik <tahmidsadik112@gmail.com>
2018-02-01 19:55:30 +0600

    Fixes typos

--- vector/vector_math.py
+++ vector/vector_math.py
@@ -1,15 +1,15 @@
 from __future__ import division
-from Vector import Vector
+from vector import Vector
 
 
 def add(v1, v2):
     if isinstance(v1, Vector) and isinstance(v2, Vector):
-        return Vector(map(lambda x, y: x + y, v1.getPoints, v2.getPoints))
+        return Vector(map(lambda x, y: x + y, v1.getPoints(), v2.getPoints()))
 
 
 def negativeVector(v):
     if isinstance(v, Vector):
-        return Vector(map(lambda x: -x, v.getPoints))
+        return Vector(map(lambda x: -x, v.getPoints()))
 
 
 def subtract(v1, v2):
@@ -18,7 +18,7 @@ def subtract(v1, v2):
 
 def multiply(v1, v2):
     if isinstance(v1, Vector) and isinstance(v2, Vector):
-        return Vector(map(lambda x, y: x * y, v1, v2))
+        return Vector(map(lambda x, y: x * y, v1.getPoints(), v2.getPoints()))
 
 
 def divide(v1, v2, divider):
