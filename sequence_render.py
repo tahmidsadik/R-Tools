ea440c747b5f444d6fc24ef98e9f0379a8a7d3f5
Tahmid Sadik <tahmidsadik112@gmail.com>
2018-02-02 00:22:44 +0600

    Adds cameraName and start frame and end frame times for the sequence render

--- sequence_render.py
+++ sequence_render.py
@@ -13,10 +13,17 @@ RENDERER = MENTAL_RAY
 
 
 def createRenderStringFromShot(shot):
-    render_cmd_string = RENDER_COMMAND + SPACE + '-r' + SPACE + RENDERER + SPACE
-    + '-im' + SPACE + 'shot_name' + SPACE + '-of' + IMAGE_EXTENSION + SPACE +
-    '-fnc' + SPACE + FNC_OPTION + '-pad' + SPACE + PADDING + '-proj' + SPACE +
-    PROJECT_PATH + SPACE + SCENE_PATH
+    render_cmd_string = RENDER_COMMAND + SPACE + \
+    '-r' + SPACE + RENDERER + SPACE + \
+    '-im' + SPACE + 'shot_name' + SPACE + \
+    '-of' + SPACE + IMAGE_EXTENSION + SPACE + \
+    '-fnc' + SPACE + FNC_OPTION + SPACE + \
+    '-pad' + SPACE + PADDING + SPACE + \
+    '-cam' + SPACE + shot.getCurrentCamera() + SPACE + \
+    '-s' + SPACE + shot.getStartTime() + SPACE + \
+    '-e' + SPACE + shot.getEndTime() + SPACE + \
+    '-proj' + SPACE + PROJECT_PATH + SPACE + \
+    SCENE_PATH
 
     return render_cmd_string
 
