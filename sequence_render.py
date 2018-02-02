2d8e735fe71ac779faf9e3e1152a48d170997f87
Tahmid Sadik <tahmidsadik112@gmail.com>
2018-02-02 12:37:46 +0600

    Fixes various path errors, handles windows and *nix platforms and windows separately

--- sequence_render.py
+++ sequence_render.py
@@ -1,16 +1,32 @@
+import maya.cmds as cmds
+from os.path import realpath
+from os.path import isdir
 from pymel.core.general import ls
+from subprocess import call
+import platform
 
+PROJECT_PATH = fixPath(cmds.workspace(q=True, rootDirectory=True))
+SCENE_PATH = fixPath(cmds.file(q=True, sceneName=True))
 RENDER_COMMAND = 'Render'
 MENTAL_RAY = 'mr'
 SOFTWARE_RENDER = 'sw'
-PROJECT_PATH = ''
-SCENE_PATH = ''
 PADDING = '3'
 SPACE = ' '
 IMAGE_EXTENSION = 'exr'
 FNC_OPTION = '6'
-RENDERER = MENTAL_RAY
+RENDERER = 'hw'
 
+def fixPath(path):
+    return str(realpath(path).replace(" ", "\ "))
+
+
+def getRenderCommandFileName():
+    rootDir = str(cmds.workspace(q=True, rootDirectory=True))
+    if(platform.system() == 'windows'):
+        return rootDir + '\\scripts\\' + 'renderSequence.bat'
+    else:
+        isdir(rootDir + 'scripts/renderSequence')
+        return rootDir + 'scripts/' + 'renderSequence.sh'
 
 def createRenderStringFromShot(shot):
     render_cmd_string = RENDER_COMMAND + SPACE + \
@@ -19,14 +35,20 @@ def createRenderStringFromShot(shot):
     '-of' + SPACE + IMAGE_EXTENSION + SPACE + \
     '-fnc' + SPACE + FNC_OPTION + SPACE + \
     '-pad' + SPACE + PADDING + SPACE + \
-    '-cam' + SPACE + shot.getCurrentCamera() + SPACE + \
-    '-s' + SPACE + shot.getStartTime() + SPACE + \
-    '-e' + SPACE + shot.getEndTime() + SPACE + \
+    '-cam' + SPACE + str(shot.getCurrentCamera()) + SPACE + \
+    '-s' + SPACE + str(shot.getStartTime()) + SPACE + \
+    '-e' + SPACE + str(shot.getEndTime()) + SPACE + \
     '-proj' + SPACE + PROJECT_PATH + SPACE + \
     SCENE_PATH
-
     return render_cmd_string
 
+def createSequenceRenderFile():
+    renderFile = open(getRenderCommandFileName(), 'w+')
+    for shot in ls(type='shot'):
+        renderCommand = createRenderStringFromShot(shot)
+        # renderCommands.append(createRenderStringFromShot(shot))
+        renderFile.write(renderCommand + '\n')
 
-for shot in ls(type='shot'):
-    print(createRenderStringFromShot(shot))
+    renderFile.close()
+
+createSequenceRenderFile()
